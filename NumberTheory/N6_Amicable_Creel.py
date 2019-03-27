cdb8c536da969b4f33951dd29cb47426a02880c1
Amy Creel <acreel@mail.umw.edu>
2019-03-27 15:40:38 -0400

    Final commit.

--- NumberTheory/N6_Amicable_Creel.py
+++ NumberTheory/N6_Amicable_Creel.py
@@ -4,7 +4,7 @@ N = 300
 # proper divisor function
 def sum_prop_div(n):
     prop_div = []
-    for i in range(1,n-1):
+    for i in range(1,int(n/2+1)):
         if n%i == 0:
             prop_div.append(i)
     sum_prop = sum(prop_div)
@@ -18,8 +18,7 @@ for a in range(1,N):
     d_a = sum_prop_div(a)
     for b in range(1,N):
         d_b = sum_prop_div(b)
-        if d_a == b and d_b == a and a!=b and a not in amicable_numbers:
+        if d_a == b and d_b == a and a not in amicable_numbers:
             amicable_numbers.append(a)
-            amicable_numbers.append(b)
 
 print("The amicable numbers up to", N, "are:", amicable_numbers)
